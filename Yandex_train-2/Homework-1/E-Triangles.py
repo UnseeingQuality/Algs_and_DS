59014b1ec1548d356293672164f2c19e0d0e7693
UnseeingQuality <balakinaa2016@mail.ru>
2025-07-24 20:46:37 +0500

    Аналитическое решение задачи E

--- Yandex_train-2/Homework-1/E-Triangles.py
+++ Yandex_train-2/Homework-1/E-Triangles.py
@@ -1,26 +1,28 @@
 p = int(input())
-half_p = p/2
-s_min = 10**10
-res_1 = [0,0,0]
-s_max = -1
-res_2 = [0,0,0]
 
-sup_1 = p//2 - (p+1) % 2
-inf_2 = (p//2 + 1)
-for side_1 in range(1, sup_1 + 1):
-    for side_2 in range(inf_2-side_1, p//2 + p % 2):
-        side_3 = p - side_1 - side_2
-        s = (half_p * (half_p - side_1) * (half_p - side_2) * (half_p - side_3))**0.5
-        if s < s_min:
-            s_min = s
-            res_2 = [side_1, side_2, side_3]
-        if s > s_max:
-            s_max = s
-            res_1 = [side_1, side_2, side_3]
-
-if res_2 == [0,0,0]:
+if p == 4 or p < 3:
     print(-1)
 else:
-    print(*res_1)
+    if p % 3 == 2:
+        mns1 = 2 - (p % 2)
+        mns2 = mns3 = (p - mns1) // 2
 
-    print(*res_2)
+        mxs1 = mxs2 = mxs3 = p // 3
+        mxs2 += 1
+        mxs3 += 1
+
+    if p % 3 == 1:
+        mns1 = 2 - (p % 2)
+        mns2 = mns3 = (p - mns1) // 2
+
+        mxs1 = mxs2 = mxs3 = p // 3
+        mxs1 += 1
+
+    if p % 3 == 0:
+        mns1 = 2 - (p % 2)
+        mns2 = mns3 = (p - mns1) // 2
+
+        mxs1 = mxs2 = mxs3 = p // 3
+
+    print(mxs1, mxs2, mxs3)
+    print(mns1, mns2, mns3)
