df37cf896ea553d38392b41443ebd4042bb7a2d2
UnseeingQuality <balakinaa2016@mail.ru>
2025-07-24 17:10:45 +0500

    Еще одна попытка, где более явны проходы циклы и смена буффера, но не работает

--- Yandex_train-2/Homework-1/D-Futurama.py
+++ Yandex_train-2/Homework-1/D-Futurama.py
@@ -9,73 +9,66 @@ def swap(bodies):
 heroes_cnt, swaps_cnt = list(map(int, input().split()))
 swaped_bodies = []
 swaped = set()
-cycles = [] # циклы (алгебра перестановок) обменов телами
 
 for i in range(swaps_cnt):
     swap_bodies = list(map(int, input().split()))
     swap_bodies[0] -= 1
     swap_bodies[1] -= 1 # для корректности перестановок в алгоритме
 
-    if cycles == []:
-        cycles.append(swap_bodies)
-    else:
-        for cycle_id in range(len(cycles)):
-            if cycles[cycle_id][-1] == swap_bodies[0] and cycles[cycle_id][0] != swap_bodies[1]:
-                cycles[cycle_id].append(swap_bodies[1])
-            elif cycles[cycle_id][-1] == swap_bodies[1] and cycles[cycle_id][0] != swap_bodies[0]:
-                cycles[cycle_id].append(swap_bodies[0])
-
     swaped.add(swap_bodies[0])
     swaped.add(swap_bodies[1])
     swaped_bodies.append(swap_bodies)
-
-# # Тут добавляются циклы длины = 1
-# for hero in range(heroes_cnt):
-#     if not any(hero in cycle for cycle in cycles):
-#         cycles.append([hero])
-
-# print("Начальная разбивка на циклы (кроме циклов длины 1): ", cycles)
 
 swaps = []
 heroes = [i for i in range(heroes_cnt)] # число n по индексу k - разум героя n в теле героя k
 res = heroes.copy()
 swaped = list(swaped)
-buffer_bodies = [i for i in range(heroes_cnt) if heroes[i] not in swaped] # два последних тела гарантированно не менялись
 
 # начальные обмены телами из условия
 for idx in range(swaps_cnt):
     swap(swaped_bodies[idx])
 
+# Соберем циклы перестановок
+used = [False] * heroes_cnt
+cycles = [] # циклы (алгебра перестановок) обменов телами
+for i in range(heroes_cnt):
+    if not used[i] and heroes[i] != i:
+        cycle = []
+        j = i
+        while not used[j]:
+            used[j] = True
+            cycle.append(j)
+            j = heroes[j]
+        if len(cycle) > 1:
+            cycles.append(cycle)
+
 # дебаг
 print(swaped)
-print(buffer_bodies)
 print(heroes, " Начальный обмен произошел ")
-print("Решение:")
+print("Разбиение перестановки на циклы: ", *cycles)
+print("Решение: \n")
 
-buf_ch_cnt = 0
+buf_1 = heroes_cnt - 2
+buf_2 = heroes_cnt - 1
+bufs = [buf_1, buf_2]
+is_flipped = False
 
 for cycle in cycles:
-    cycle_len = len(cycle)
-    while cycle:
-        cur_mind = cycle[0]
-        cur_buff = buffer_bodies[0]
-        buf_ch_cnt += 1
-        if buf_ch_cnt >= cycle_len:
-            buf_ch_cnt = 0
-            cur_buff = buffer_bodies[1]
-        cycle.pop(0)
-        if cur_mind == heroes.index(cur_mind) or heroes.index(cur_mind) == cur_buff:
-            continue
-        else:
-            print([heroes.index(cur_mind), cur_buff])
-            swap([heroes.index(cur_mind), cur_buff])
-            print(heroes)
-            if cur_mind == cur_buff or len(cycle) == 1:
-                continue
-            else:
-                print([cur_buff, cur_mind])
-                swap([cur_buff, cur_mind])
-                print(heroes)
+    first, second = (bufs if not is_flipped else bufs[::-1])
+
+    for body_idx in cycle:
+        print([body_idx, first])
+        swap([body_idx, first])
+        print(heroes)
+
+    if not is_flipped:
+        is_flipped = True
+        swap([first, second])
+
+    for body_idx in reversed(cycle):
+        print([body_idx, second])
+        swap([body_idx, second])
+        print(heroes)
 
 
 if heroes == res:
