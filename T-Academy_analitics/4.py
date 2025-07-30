c1b17701610b4562237709335c41ab23b1e0972c
UnseeingQuality <balakinaa2016@mail.ru>
2025-07-30 21:16:51 +0500

    решение задачи 4

--- T-Academy_analitics/4.py
+++ T-Academy_analitics/4.py
@@ -1,22 +1,34 @@
 N = int(input())
-S = str(input())
-A = [-1 for i in range(N+1)]
-idxs = [-1 for i in range(N+1)] # индекс - число в массиве, значение - его индекс в массиве
+S = input().strip()
 
-idxs[0] = 0
-for i in range(N):
-    opr = S[i]
-    num = i+1
-    if opr == "L":
-        idxs[num] = idxs[num-1]
-        for j in range(num):
-            idxs[j] += 1
-    if opr == "R":
-        idxs[num] = idxs[num-1]
-        for j in range(num):
-            idxs[j] -= 1
+L = [-1] * (N + 1)
+R = [-1] * (N + 1)
+head = 0
 
-for i in range(N+1):
-    A[idxs[i]] = i
+for i, c in enumerate(S, start=1):
+    p = i - 1
+    if c == 'L':
+        prev = L[p]
 
-print(*A)
+        L[i] = prev
+        R[i] = p
+        L[p] = i
+        if prev != -1:
+            R[prev] = i
+        else:
+            head = i
+    else:
+        nxt = R[p]
+        L[i] = p
+        R[i] = nxt
+        R[p] = i
+        if nxt != -1:
+            L[nxt] = i
+
+res = []
+v = head
+while v != -1:
+    res.append(v)
+    v = R[v]
+
+print(*res)
