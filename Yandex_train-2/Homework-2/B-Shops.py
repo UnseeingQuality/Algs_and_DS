098c3f41b388dffc24c079c2022d13a2bb702e19
UnseeingQuality <balakinaa2016@mail.ru>
2025-08-12 16:05:50 +0500

    right solve for 2 passages

--- Yandex_train-2/Homework-2/B-Shops.py
+++ Yandex_train-2/Homework-2/B-Shops.py
@@ -1,27 +1,30 @@
 def find_mx_left_dist(arr):
+    dists = [-1 for _ in range(len(arr))]
     mx_dist = -1
     cur_dist = 0
     is_find_home = False
+    is_find_shop = False
     for i in range(len(arr)):
         cur = arr[i]
         if cur == 2:
-            if is_find_home:
-                mx_dist = max(mx_dist, cur_dist)
+            is_find_shop = True
             cur_dist = 0
         elif cur == 1:
-            is_find_home = True
-            cur_dist += 1
-            mx_dist = max(mx_dist, cur_dist)
+            if is_find_shop:
+                cur_dist += 1
+                dists[i] = cur_dist
+            else:
+                dists[i] = len(arr) + 1
         elif cur == 0:
-            mx_dist = max(mx_dist, cur_dist)
             cur_dist += 1
-            is_find_home = False
-    return mx_dist
+    return dists
 
 
 city = list(map(int, input().split()))
 l_res = find_mx_left_dist(city)
-r_res = find_mx_left_dist(city[::-1])
+r_res = find_mx_left_dist(city[::-1])[::-1]
+# print(*l_res)
+# print(*r_res)
 
-res = min(l_res, r_res)
-print(res)
+res = [min(l_res[i], r_res[i]) for i in range(len(city))]
+print(max(res))
